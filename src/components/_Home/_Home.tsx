4b637fb0dc5a9316e04df34704de332e78393c61
Troy Marchesseault <tmarchess@gmail.com>
2023-06-21 17:10:20 -0400

    Create skeleton for Home page

--- src/components/_Home/_Home.tsx
+++ src/components/_Home/_Home.tsx
@@ -1,4 +1,5 @@
 import Cover from "../generic/Cover";
+import Footer from "../generic/Footer";
 import Hero from "./Hero";
 
 function HomePage({
@@ -9,34 +10,90 @@ function HomePage({
     setSearchBarState: (state: "ACTIVE" | "INACTIVE") => void;
 }) {
     return (
-        <div className="relative flex-1 bg-gradient-to-bl from-neutral-500 via-white to-white">
-            <div className="max-w-[1440px] m-auto">
-                <div className="sm:p-8 h-[calc(100vh-3.5rem)]">
-                    <Hero />
-                    <div className="flex flex-col sm:flex-row gap-4 sm:gap-8 mt-4 sm:h-12 text-xl sm:text-3xl font-bold items-center">
-                        <div className="text-neutral-600">
-                            Check out the presentation
-                        </div>
-                        <a
-                            href="https://www.youtube.com/watch?v=lINJW1oRYJA"
-                            target="_blank"
-                            className="py-2 px-8 bg-bintendo text-white font-normal text-base sm:text-xl rounded
-                        sm:hover:bg-red-800 sm:hover:py-3 sm:hover:px-9 sm:hover:-ml-1 ease-in-out duration-300
-                        "
-                        >
-                            Watch now
-                        </a>
-                    </div>
-                </div>
-                <hr />
-                <div className="text-3xl font-bold m-20">
-                    <div className="text-neutral-600">Featured</div>
-                </div>
-            </div>
+        <div className="relative flex-1 ">
             <Cover
                 searchBarState={searchBarState}
                 setSearchBarState={setSearchBarState}
             />
+            {/*Hero Section*/}
+            <div className="bg-gradient-to-bl from-neutral-500 via-white to-white">
+                <div className="max-w-[1440px] mx-auto">
+                    <div className="sm:p-8 h-[calc(100vh-3.5rem)]">
+                        <Hero />
+                        <div className="flex flex-col sm:flex-row gap-4 sm:gap-8 mt-4 sm:h-12 text-xl sm:text-3xl font-bold items-center">
+                            <div className="text-neutral-600">
+                                Check out the presentation
+                            </div>
+                            <a
+                                href="https://www.youtube.com/watch?v=lINJW1oRYJA"
+                                target="_blank"
+                                className="py-2 px-8 bg-bintendo text-white font-normal text-base sm:text-xl rounded
+                        sm:hover:bg-red-800 sm:hover:py-3 sm:hover:px-9 sm:hover:-ml-1 ease-in-out duration-300
+                        "
+                            >
+                                Watch now
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr />
+            {/*Featured content section*/}
+            <section className="max-w-[1144px] my-8 mx-auto text-3xl text-neutral-600">
+                <div className="">
+                    <div className="font-bold mb-4">Featured</div>
+                    <div className="sm:h-[340px] bg-bintendo"></div>
+                </div>
+            </section>
+            <hr />
+            {/*Online store section*/}
+            <section className="max-w-[1144px] my-8 mx-auto text-3xl text-neutral-600">
+                <div className="">
+                    <div className="font-bold mb-4">Online Store</div>
+                    <div className="sm:h-[340px] bg-bintendo"></div>
+                </div>
+            </section>
+            <hr />
+            {/*Garbage System Section*/}
+            <section className="max-w-[1144px] my-8 mx-auto text-3xl text-neutral-600">
+                <div className="">
+                    <div className="font-bold mb-4">Garbage Systems</div>
+                    <div className="sm:h-[340px] bg-bintendo"></div>
+                </div>
+            </section>
+            <hr />
+            {/*News Section*/}
+            <section className="max-w-[1144px] my-8 mx-auto text-3xl text-neutral-600">
+                <div className="">
+                    <div className="font-bold mb-4">News</div>
+                    <div className="sm:h-[340px] bg-bintendo"></div>
+                </div>
+            </section>
+            <hr />
+            {/*Characters Section*/}
+            <section className="max-w-[1144px] my-8 mx-auto text-3xl text-neutral-600">
+                <div className="">
+                    <div className="font-bold mb-4">Characters</div>
+                    <div className="sm:h-[340px] bg-bintendo"></div>
+                </div>
+            </section>
+            <hr />
+            {/*Best Sellers Section*/}
+            <section className="max-w-[1144px] my-8 mx-auto text-3xl text-neutral-600">
+                <div className="">
+                    <div className="font-bold mb-4">Best Sellers</div>
+                    <div className="sm:h-[340px] bg-bintendo"></div>
+                </div>
+            </section>
+            <hr />
+            {/*New Releases Section*/}
+            <section className="max-w-[1144px] my-8 mx-auto text-3xl text-neutral-600">
+                <div className="">
+                    <div className="font-bold mb-4">New Releases</div>
+                    <div className="sm:h-[340px] bg-bintendo"></div>
+                </div>
+            </section>
+            <Footer />
         </div>
     );
 }
